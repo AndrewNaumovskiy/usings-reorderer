b7710ad9f2c5193f8b317a1468b4dbdd36402600
Naumovskyi Andrii <andrii.naumovskyi@auto.works>
2025-08-23 16:07:06 +0300

    Small adjustments

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Usage: mytool <file>");
+        eprintln!("Usage: usings-reorderer <file>");
         return;
     }
 
@@ -17,40 +17,33 @@ fn main() {
     let content = fs::read_to_string(path).expect("Failed to read file");
     let lines = content.lines();
 
-    // find index of first line without using
-    let first_non_using_line_index = lines.clone().position(|line| !line.contains("using "));
-
-    // get first 5 lines
-    let using_lines: Vec<&str> = lines
-        .clone()
-        .take(first_non_using_line_index.unwrap_or(0))
+    let using_lines: Vec<String> = lines
+        .filter(|line| line.contains("using "))
+        .map(|line| line.replace('\u{feff}', ""))
         .collect();
 
-    // group usings by first word in split by '.' ("System.IO")
-    let mut grouped_usings: HashMap<&str, Vec<&str>> = HashMap::new();
-    for line in using_lines {
-        let namespace = line.split_whitespace().nth(1).unwrap_or("");
-        if namespace.contains('.') {
-            let first_word = namespace.split('.').next().unwrap_or("");
+    let mut grouped_usings: HashMap<String, Vec<String>> = HashMap::new();
 
-            grouped_usings
-                .entry(first_word)
-                .or_insert_with(Vec::new)
-                .push(line);
+    for using in using_lines {
+        let namespace = using.split_whitespace().nth(1).unwrap_or("").to_string();
+
+        let cleaned_namespace = if namespace.contains('.') {
+            namespace.split('.').next().unwrap_or("").to_string()
         } else {
-            let cleaned_namespace = namespace.split(";").next().unwrap_or("");
-            grouped_usings
-                .entry(cleaned_namespace)
-                .or_insert_with(Vec::new)
-                .push(line);
-        }
+            namespace.split(';').next().unwrap_or("").to_string()
+        };
+
+        grouped_usings
+            .entry(cleaned_namespace)
+            .or_insert_with(Vec::new)
+            .push(using);
     }
 
     // sort by grouped_usings by key length
     let mut sorted_grouped_usings: Vec<_> = grouped_usings.into_iter().collect();
     sorted_grouped_usings.sort_by_key(|(key, _)| key.len());
 
-    let mut final_sorted_usings = vec![];
+    let mut final_sorted_usings: Vec<String> = vec![];
 
     for (_first_word, mut lines) in sorted_grouped_usings {
         // sort lines by length
@@ -62,7 +55,9 @@ fn main() {
 
     // overwrite path first lines with final_sorted_usings
     let mut content_lines = content.lines().collect::<Vec<_>>();
-    content_lines.splice(0..final_sorted_usings.len(), final_sorted_usings);
+    let final_sorted_usings_refs: Vec<&str> =
+        final_sorted_usings.iter().map(|s| s.as_str()).collect();
+    content_lines.splice(0..final_sorted_usings_refs.len(), final_sorted_usings_refs);
     let new_content = content_lines.join("\n");
     fs::write(path, new_content).expect("Failed to write file");
 }
