cfe391a611dfa981430e3bed2d7a11569ed91a1e
Naumovskyi Andrii <andrii.naumovskyi@auto.works>
2025-08-25 09:32:33 +0300

    Fix iteration over non namespace usings. Write a new line at the end of a file

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,21 @@ fn main() {
 
     let path = &args[1];
 
-    //let path = "C:\\AutoloadItRepos\\Desktop\\AutoLoadIT\\AutoLoadIT\\ViewModels\\Tabs\\DocumentTabViewModel.cs";
-
     // read lines from the file
     let content = fs::read_to_string(path).expect("Failed to read file");
     let lines = content.lines();
 
-    let using_lines: Vec<String> = lines
-        .filter(|line| line.contains("using "))
-        .map(|line| line.replace('\u{feff}', ""))
-        .collect();
+    let mut using_lines: Vec<String> = Vec::new();
+
+    for line in lines {
+        if line.is_empty() {
+            break;
+        }
+
+        if line.contains("using ") {
+            using_lines.push(line.replace('\u{feff}', ""));
+        }
+    }
 
     let mut grouped_usings: HashMap<String, Vec<String>> = HashMap::new();
 
@@ -46,7 +51,7 @@ fn main() {
 
     let mut final_sorted_usings: Vec<String> = vec![];
 
-    for (_first_word, mut lines) in sorted_grouped_usings {
+    for (_, mut lines) in sorted_grouped_usings {
         // sort lines by length
         lines.sort_by_key(|line| line.len());
         for line in lines {
@@ -59,6 +64,7 @@ fn main() {
     let final_sorted_usings_refs: Vec<&str> =
         final_sorted_usings.iter().map(|s| s.as_str()).collect();
     content_lines.splice(0..final_sorted_usings_refs.len(), final_sorted_usings_refs);
-    let new_content = content_lines.join("\n");
+    let mut new_content = content_lines.join("\n");
+    new_content.push('\n');
     fs::write(path, new_content).expect("Failed to write file");
 }
